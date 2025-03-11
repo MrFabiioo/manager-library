f7d703cbbc95fc3bdd73c06178debf211d5f7463
Fabio Ortega <fabiolin57@gmail.com>
2025-03-11 18:20:11 -0500

    depurando con vercel build

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -88,7 +88,7 @@ export default function NavBar(){
     </div>
   </div>
 
-  <div className="hidden" id="mobile-menu">
+  <div className="sm:hidden" id="mobile-menu">
     <div className="space-y-1 px-2 pt-2 pb-3">
       <a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
       <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
