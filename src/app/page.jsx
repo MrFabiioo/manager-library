0c549605e82f4482862cf8ae42aa75e9aa58d6eb
Fabio Ortega <fabiolin57@gmail.com>
2025-03-11 16:19:38 -0500

    cambiando a main para seguir

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -33,7 +33,7 @@ export default function Home() {
               <span className="sr-only">Your Company</span>
               <Image
                 alt="imagen" width={100} height={50} 
-                src="/images/creo.png"
+                src="/images/Creo.png"
                 className="h-8 w-auto rounded-xl"
               />
             </a>
