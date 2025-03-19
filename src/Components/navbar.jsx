97220307178794b9be9e0f389757cbf153e290fd
Fabio Ortega <fabiolin57@gmail.com>
2025-03-18 22:07:48 -0500

    target blank a menu hamburguesa

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -93,7 +93,7 @@ export default function NavBar(){
     openHamburguerMenu &&
     <div className="sm:hidden" id="mobile-menu">
     <div className="space-y-1 px-2 pt-2 pb-3">
-      <a href="https://biblioteca-personal.vercel.app" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Biblioteca</a>
+      <a target="_blank" href="https://biblioteca-personal.vercel.app" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Biblioteca</a>
       <a href="/Libros" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Libros</a>
       <a href="/categorias" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Categorias</a>
       <a href="/resenas" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reseñas</a>
